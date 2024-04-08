724239c65ce6679f4b01878d969b7d9a781144e3
mlazoy <mlazoy@icloud.com>
2024-04-08 11:55:42 +0300

    added clk an rst to full_adder- new testbench

--- lab_3/full_adder.vhd
+++ lab_3/full_adder.vhd
@@ -5,7 +5,7 @@ use IEEE.STD_LOGIC_1164.ALL;
 
 
 entity full_adder is
-    port ( Ain, Bin, Cin: in std_logic;
+    port ( Ain, Bin, Cin, clk, rst: in std_logic;
            Sum, Cout: out std_logic);
 end full_adder;
 
@@ -14,35 +14,39 @@ signal input: std_logic_vector(2 downto 0);
 signal output: std_logic_vector(1 downto 0);
 
 begin
-process (Ain,Bin,Cin) begin
-    input(2)<=Ain;
-    input(1)<=Bin;
-    input(0)<=Cin;
-    
-    case (input) is
-         when "000" => 
-            output<="00";
-         when "001" =>
-            output<="10";
-         when "010" =>
-            output<="10";
-         when "011" =>
-            output<="01";
-         when "100" =>
-            output<="10";
-         when "101" =>
-            output<="01";
-         when "110" =>
-            output<="01";
-         when "111" =>
-            output<="11";
-         when others =>
-            null;     
-    end case;  
+
+process(clk) begin
+
+    input<=Ain&Bin&Cin; 
+    if rst='1' then
+          output<="00";
+    elsif clk'event and clk='1' then 
+        case (input) is
+             when "000" => 
+                output<="00";
+             when "001" =>
+                output<="10";
+             when "010" =>
+                output<="10";
+             when "011" =>
+                output<="01";
+             when "100" =>
+                output<="10";
+             when "101" =>
+                output<="01";
+             when "110" =>
+                output<="01";
+             when "111" =>
+                output<="11";  
+             when others =>
+                output<="ZZ";  
+        end case;
+    end if;
 
 end process;
-     
-Sum<=output(1);
-Cout<=output(0); 
 
+Sum<=output(1);
+Cout<=output(0);     
+  
 end Behavioral;
+
--- lab_3/full_adder_tb.vhd
+++ lab_3/full_adder_tb.vhd
@@ -9,40 +9,57 @@ end full_adder_tb;
 architecture TestBench of full_adder_tb is
 
 component full_adder is 
-    port (Ain,Bin,Cin: in std_logic;
+    port (Ain,Bin,Cin, clk, rst: in std_logic;
           Sum, Cout: out std_logic);
 end component;
 
-signal Ain_tb,Bin_tb,Cin_tb,Sum_tb,Cout_tb: std_logic;
+signal Ain_tb,Bin_tb,Cin_tb,Sum_tb,Cout_tb,clk_tb,rst_tb: std_logic;
 
 begin
 
 FA: full_adder port map (Ain=>Ain_tb,
                          Bin=>Bin_tb,
                          Cin=>Cin_tb,
+                         clk=>clk_tb,
+                         rst=>rst_tb,
                          Sum=>Sum_tb,
                          Cout=>Cout_tb);
+                         
+
 sim: process 
 begin
+--reset is on
+clk_tb<='0';
+rst_tb<='1';
+wait for 10ns;
+clk_tb<='1';
+wait for 10ns;
 
-    for a in std_logic range '0' to '1' loop
-        for b in std_logic range '0' to '1' loop
-            for c in std_logic range '0' to '1' loop
-                Ain_tb<=a;
-                Bin_tb<=b;
-                Cin_tb<=c;
-                
-                --assert Sum_tb=((a xor b) xor c) report "Incorrect sum" severity error;
-                --assert Cout_tb=((a and b) or (a and c) or (b and c)) report "Incorrect carry" severity error;
-                
-                wait for 10ns;
-                
-            end loop;
+--reset is off
+rst_tb<='0';
+
+   for a in std_logic range '0' to '1' loop
+     for b in std_logic range '0' to '1' loop
+        for c in std_logic range '0' to '1' loop
+            Ain_tb<=a;
+            Bin_tb<=b;
+            Cin_tb<=c;
+            clk_tb<='0';
+            wait for 10ns;
+            clk_tb<='1';
+            if clk_tb='1' then 
+                assert Sum_tb=((a xor b) xor c) report "Incorrect sum" severity error;
+                assert Cout_tb=((a and b) or (a and c) or (b and c)) report "Incorrect carry" severity error;
+            end if;
+            wait for 10ns;
         end loop;
      end loop;
-
+   end loop;
 
 end process;                  
 
 
 end TestBench;
+
+
+
