d483ceced220284f01e0e8e7e5e1cf9bf1857d92
mlazoy <mlazoy@icloud.com>
2024-04-10 10:46:14 +0300

    CC fix

--- lab_3/mult_4bits.vhd
+++ lab_3/mult_4bits.vhd
@@ -3,7 +3,7 @@ use ieee.std_logic_1164.all;
 
 entity mult_4bits is
     port (num_A, num_B: in std_logic_vector(3 downto 0);
-            Ci, pulse: std_logic;
+            pulse: std_logic;
             prod: out std_logic_vector(7 downto 0));
 end mult_4bits;
 
@@ -21,7 +21,7 @@ signal a3b0, a2b0, a1b0, a0b0,
 
 
 signal buffer_A0, buffer_A1, buffer_A2, buffer_A3,
-        buffer_B0, buffer_B1, buffer_B2, buffer_B3: std_logic_vector(9 downto 0);
+        buffer_B0, buffer_B1, buffer_B2, buffer_B3: std_logic_vector(8 downto 0);
 
 signal tmp_prod_0: std_logic_vector(9 downto 0);
 signal tmp_prod_1: std_logic_vector(6 downto 0);
@@ -118,7 +118,7 @@ FA_21: full_adder port map (
 a3b1 <= buffer_A3(4) and buffer_B1(4);
 FA_31: full_adder port map (
     A=>a3b1,
-    B=>carry_row_0(3),
+    B=>carry_row_0(3), 
     Cin=>carry_row_1(2),
     clk=>pulse,
     Sum=>p_sum_4(0),
@@ -126,7 +126,7 @@ FA_31: full_adder port map (
 );
 
 --A0*B2
-a0b2 <= buffer_A0(2) and buffer_B2(2);
+a0b2 <= buffer_A0(3) and buffer_B2(3);
 FA_02: full_adder port map (
     A=>a0b2,
     B=>p_sum_2(1), 
@@ -137,7 +137,7 @@ FA_02: full_adder port map (
 );
 
 --A1*B2
-a1b2 <= buffer_A1(3) and buffer_B2(3);
+a1b2 <= buffer_A1(4) and buffer_B2(4);
 FA_12: full_adder port map (
     A=>a1b2,
     B=>p_sum_3(1),
@@ -148,7 +148,7 @@ FA_12: full_adder port map (
 );
 
 --A2*B2
-a2b2 <= buffer_A2(4) and buffer_B2(4);
+a2b2 <= buffer_A2(5) and buffer_B2(5);
 FA_22: full_adder port map (
     A=>a2b2,
     B=>p_sum_4(0),
@@ -159,7 +159,7 @@ FA_22: full_adder port map (
 );
 
 --A3*B2
-a3b2 <= buffer_A3(5) and buffer_B2(5);
+a3b2 <= buffer_A3(6) and buffer_B2(6);
 FA_32: full_adder port map (
     A=>a3b2,
     B=>carry_row_1(3),
@@ -170,7 +170,7 @@ FA_32: full_adder port map (
 );
 
 --A0*B3
-a0b3 <= buffer_A0(4) and buffer_B3(4);
+a0b3 <= buffer_A0(5) and buffer_B3(5);
 FA_03: full_adder port map (
     A=>a0b3,
     B=>p_sum_3(2),
@@ -181,7 +181,7 @@ FA_03: full_adder port map (
 );
 
 --A1*B3
-a1b3 <= buffer_A1(5) and buffer_B3(5);
+a1b3 <= buffer_A1(6) and buffer_B3(6);
 FA_13: full_adder port map (
     A=>a1b3,
     B=>p_sum_4(1),
@@ -192,7 +192,7 @@ FA_13: full_adder port map (
 );
 
 --A2*B3
-a2b3 <= buffer_A2(6) and buffer_B3(6);
+a2b3 <= buffer_A2(7) and buffer_B3(7);
 FA_23: full_adder port map (
     A=>a2b3,
     B=>p_sum_5,
@@ -203,7 +203,7 @@ FA_23: full_adder port map (
 );
 
 --A3*B3
-a3b3 <= buffer_A3(7) and buffer_B3(7);
+a3b3 <= buffer_A3(8) and buffer_B3(8);
 FA_33: full_adder port map (
     A=>a3b3,
     B=>carry_row_2(3),
@@ -219,15 +219,15 @@ process(pulse) begin
         prod(5 downto 0)<=tmp_prod_0(9) & tmp_prod_1(6) & tmp_prod_2(4) & tmp_prod_3(2) & tmp_prod_4(1);
     
         --shift all buffers and tmp_prod signals right
-        buffer_A0(9 downto 1)<=buffer_A0(8 downto 0);
-        buffer_A1(9 downto 1)<=buffer_A1(8 downto 0);
-        buffer_A2(9 downto 1)<=buffer_A2(8 downto 0);
-        buffer_A3(9 downto 1)<=buffer_A3(8 downto 0);
+        buffer_A0(8 downto 1)<=buffer_A0(7 downto 0);
+        buffer_A1(8 downto 1)<=buffer_A1(7 downto 0);
+        buffer_A2(8 downto 1)<=buffer_A2(7 downto 0);
+        buffer_A3(8 downto 1)<=buffer_A3(7 downto 0);
     
-        buffer_B0(9 downto 1)<=buffer_B0(8 downto 0);
-        buffer_B1(9 downto 1)<=buffer_B1(8 downto 0);
-        buffer_B2(9 downto 1)<=buffer_B2(8 downto 0);
-        buffer_B3(9 downto 1)<=buffer_B3(8 downto 0);
+        buffer_B0(8 downto 1)<=buffer_B0(7 downto 0);
+        buffer_B1(8 downto 1)<=buffer_B1(7 downto 0);
+        buffer_B2(8 downto 1)<=buffer_B2(7 downto 0);
+        buffer_B3(8 downto 1)<=buffer_B3(7 downto 0);
     
         tmp_prod_0(9 downto 1)<=tmp_prod_0(8 downto 0);
         tmp_prod_1(6 downto 1)<=tmp_prod_1(5 downto 0);
