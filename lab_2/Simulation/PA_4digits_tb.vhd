c1196d56252b1be322ba9862d1bc8253641537fd
mlazoy <mlazoy@icloud.com>
2024-04-12 12:20:29 +0300

    tb final

--- lab_2/Simulation/PA_4digits_tb.vhd
+++ lab_2/Simulation/PA_4digits_tb.vhd
@@ -41,20 +41,22 @@ uut: digital4_adder port map (A1=>A1_tb,
 
 sim: process
 begin
-for i in 0 to 9999 loop
-    for j in 0 to 9999 loop
+for i in 9990 to 9999 loop
+    for j in 5476 to 9999 loop
         num_A<=std_logic_vector(to_unsigned(i,16));
         num_B<=std_logic_vector(to_unsigned(j,16));
         A1_tb<=std_logic_vector(to_unsigned(i mod 10,4));
         B1_tb<=std_logic_vector(to_unsigned(j mod 10,4));
         A2_tb<=std_logic_vector(to_unsigned((i/10) mod 10,4));
-        B2_tb<=std_logic_vector(to_unsigned((i/10) mod 10,4));
+        B2_tb<=std_logic_vector(to_unsigned((j/10) mod 10,4));
         A3_tb<=std_logic_vector(to_unsigned((i/100) mod 10,4));
-        B3_tb<=std_logic_vector(to_unsigned((i/100) mod 10,4));
+        B3_tb<=std_logic_vector(to_unsigned((j/100) mod 10,4));
         A4_tb<=std_logic_vector(to_unsigned((i/1000) mod 10,4));
-        B4_tb<=std_logic_vector(to_unsigned((i/1000) mod 10,4));
-        result<=S4_tb & S3_tb & S2_tb & S1_tb;
-        wait for 10ns;
+        B4_tb<=std_logic_vector(to_unsigned((j/1000) mod 10,4));
+         wait for 10ns;
+        result<=std_logic_vector(to_unsigned((to_integer(unsigned(S4_tb))*1000 + to_integer(unsigned(S3_tb))*100 + 
+                to_integer(unsigned(S2_tb))*10 + to_integer(unsigned(S1_tb))),16));  
+        wait for 10ns;      
     end loop;
 end loop;
 wait;
