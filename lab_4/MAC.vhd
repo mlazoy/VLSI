ad3ee8e62e858510e37dcd4126f8b1fac7306d70
mlazoy <mlazoy@icloud.com>
2024-04-15 12:35:24 +0300

    mac

--- lab_4/MAC.vhd
+++ lab_4/MAC.vhd
@@ -4,11 +4,21 @@ use ieee.std_logic_unsigned.all;
 
 entity MAC is 
     port (rom_out, ram_out: in std_logic_vector(7 downto 0);
-          init: in std_logic;
+          clk, init: in std_logic;
           accumulator: out std_logic_vector(16 downto 0)); -- 8 bits + 8 bits + 1 
 end MAC;
 
 architecture Behavioral of MAC is
 begin
+    process(clk, init)
+    begin 
+        if clk'event and clk = '1' then
+            if init = '1' then
+                accumulator <= (others=>'0');
+            end if;
+            accumulator <= accumulator + rom_out*ram_out;
+        end if;
+
+    end process;
 
 end Behavioral;
