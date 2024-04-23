2a8fea86650abbc5aa01cb5b922cb2a52ef044b9
mlazoy <mlazoy@icloud.com>
2024-04-23 20:12:36 +0300

    Axi repo and block design project

---
+++ lab_5/lab_5.srcs/sources_1/bd/design_1/ip/design_1_fir_IP_0_0/design_1_fir_IP_0_0_sim_netlist.vhdl
@@ -0,0 +1,324 @@
+-- Copyright 1986-2018 Xilinx, Inc. All Rights Reserved.
+-- --------------------------------------------------------------------------------
+-- Tool Version: Vivado v.2018.2 (lin64) Build 2258646 Thu Jun 14 20:02:38 MDT 2018
+-- Date        : Tue Apr 23 20:05:40 2024
+-- Host        : mlazoy-vm running 64-bit Ubuntu 22.04.4 LTS
+-- Command     : write_vhdl -force -mode funcsim
+--               /home/mlazoy/lab_5/lab_5.srcs/sources_1/bd/design_1/ip/design_1_fir_IP_0_0/design_1_fir_IP_0_0_sim_netlist.vhdl
+-- Design      : design_1_fir_IP_0_0
+-- Purpose     : This VHDL netlist is a functional simulation representation of the design and should not be modified or
+--               synthesized. This netlist cannot be used for SDF annotated simulation.
+-- Device      : xc7z010clg400-1
+-- --------------------------------------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+library UNISIM;
+use UNISIM.VCOMPONENTS.ALL;
+entity design_1_fir_IP_0_0_fir_IP_v1_0_S00_AXI is
+  port (
+    s00_axi_wready : out STD_LOGIC;
+    s00_axi_awready : out STD_LOGIC;
+    s00_axi_arready : out STD_LOGIC;
+    s00_axi_bvalid : out STD_LOGIC;
+    s00_axi_rvalid : out STD_LOGIC;
+    s00_axi_aclk : in STD_LOGIC;
+    s00_axi_aresetn : in STD_LOGIC;
+    s00_axi_wvalid : in STD_LOGIC;
+    s00_axi_awvalid : in STD_LOGIC;
+    s00_axi_bready : in STD_LOGIC;
+    s00_axi_arvalid : in STD_LOGIC;
+    s00_axi_rready : in STD_LOGIC
+  );
+  attribute ORIG_REF_NAME : string;
+  attribute ORIG_REF_NAME of design_1_fir_IP_0_0_fir_IP_v1_0_S00_AXI : entity is "fir_IP_v1_0_S00_AXI";
+end design_1_fir_IP_0_0_fir_IP_v1_0_S00_AXI;
+
+architecture STRUCTURE of design_1_fir_IP_0_0_fir_IP_v1_0_S00_AXI is
+  signal axi_arready0 : STD_LOGIC;
+  signal axi_awready0 : STD_LOGIC;
+  signal axi_bvalid_i_1_n_0 : STD_LOGIC;
+  signal axi_rvalid_i_1_n_0 : STD_LOGIC;
+  signal axi_wready0 : STD_LOGIC;
+  signal p_0_in : STD_LOGIC;
+  signal \^s00_axi_arready\ : STD_LOGIC;
+  signal \^s00_axi_awready\ : STD_LOGIC;
+  signal \^s00_axi_bvalid\ : STD_LOGIC;
+  signal \^s00_axi_rvalid\ : STD_LOGIC;
+  signal \^s00_axi_wready\ : STD_LOGIC;
+  attribute SOFT_HLUTNM : string;
+  attribute SOFT_HLUTNM of axi_arready_i_1 : label is "soft_lutpair0";
+  attribute SOFT_HLUTNM of axi_awready_i_2 : label is "soft_lutpair1";
+  attribute SOFT_HLUTNM of axi_rvalid_i_1 : label is "soft_lutpair0";
+  attribute SOFT_HLUTNM of axi_wready_i_1 : label is "soft_lutpair1";
+begin
+  s00_axi_arready <= \^s00_axi_arready\;
+  s00_axi_awready <= \^s00_axi_awready\;
+  s00_axi_bvalid <= \^s00_axi_bvalid\;
+  s00_axi_rvalid <= \^s00_axi_rvalid\;
+  s00_axi_wready <= \^s00_axi_wready\;
+axi_arready_i_1: unisim.vcomponents.LUT2
+    generic map(
+      INIT => X"2"
+    )
+        port map (
+      I0 => s00_axi_arvalid,
+      I1 => \^s00_axi_arready\,
+      O => axi_arready0
+    );
+axi_arready_reg: unisim.vcomponents.FDRE
+     port map (
+      C => s00_axi_aclk,
+      CE => '1',
+      D => axi_arready0,
+      Q => \^s00_axi_arready\,
+      R => p_0_in
+    );
+axi_awready_i_1: unisim.vcomponents.LUT1
+    generic map(
+      INIT => X"1"
+    )
+        port map (
+      I0 => s00_axi_aresetn,
+      O => p_0_in
+    );
+axi_awready_i_2: unisim.vcomponents.LUT3
+    generic map(
+      INIT => X"08"
+    )
+        port map (
+      I0 => s00_axi_wvalid,
+      I1 => s00_axi_awvalid,
+      I2 => \^s00_axi_awready\,
+      O => axi_awready0
+    );
+axi_awready_reg: unisim.vcomponents.FDRE
+     port map (
+      C => s00_axi_aclk,
+      CE => '1',
+      D => axi_awready0,
+      Q => \^s00_axi_awready\,
+      R => p_0_in
+    );
+axi_bvalid_i_1: unisim.vcomponents.LUT6
+    generic map(
+      INIT => X"0000FFFF80008000"
+    )
+        port map (
+      I0 => \^s00_axi_awready\,
+      I1 => s00_axi_wvalid,
+      I2 => s00_axi_awvalid,
+      I3 => \^s00_axi_wready\,
+      I4 => s00_axi_bready,
+      I5 => \^s00_axi_bvalid\,
+      O => axi_bvalid_i_1_n_0
+    );
+axi_bvalid_reg: unisim.vcomponents.FDRE
+     port map (
+      C => s00_axi_aclk,
+      CE => '1',
+      D => axi_bvalid_i_1_n_0,
+      Q => \^s00_axi_bvalid\,
+      R => p_0_in
+    );
+axi_rvalid_i_1: unisim.vcomponents.LUT4
+    generic map(
+      INIT => X"08F8"
+    )
+        port map (
+      I0 => s00_axi_arvalid,
+      I1 => \^s00_axi_arready\,
+      I2 => \^s00_axi_rvalid\,
+      I3 => s00_axi_rready,
+      O => axi_rvalid_i_1_n_0
+    );
+axi_rvalid_reg: unisim.vcomponents.FDRE
+     port map (
+      C => s00_axi_aclk,
+      CE => '1',
+      D => axi_rvalid_i_1_n_0,
+      Q => \^s00_axi_rvalid\,
+      R => p_0_in
+    );
+axi_wready_i_1: unisim.vcomponents.LUT3
+    generic map(
+      INIT => X"08"
+    )
+        port map (
+      I0 => s00_axi_wvalid,
+      I1 => s00_axi_awvalid,
+      I2 => \^s00_axi_wready\,
+      O => axi_wready0
+    );
+axi_wready_reg: unisim.vcomponents.FDRE
+     port map (
+      C => s00_axi_aclk,
+      CE => '1',
+      D => axi_wready0,
+      Q => \^s00_axi_wready\,
+      R => p_0_in
+    );
+end STRUCTURE;
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+library UNISIM;
+use UNISIM.VCOMPONENTS.ALL;
+entity design_1_fir_IP_0_0_fir_IP_v1_0 is
+  port (
+    s00_axi_wready : out STD_LOGIC;
+    s00_axi_awready : out STD_LOGIC;
+    s00_axi_arready : out STD_LOGIC;
+    s00_axi_bvalid : out STD_LOGIC;
+    s00_axi_rvalid : out STD_LOGIC;
+    s00_axi_aclk : in STD_LOGIC;
+    s00_axi_aresetn : in STD_LOGIC;
+    s00_axi_wvalid : in STD_LOGIC;
+    s00_axi_awvalid : in STD_LOGIC;
+    s00_axi_bready : in STD_LOGIC;
+    s00_axi_arvalid : in STD_LOGIC;
+    s00_axi_rready : in STD_LOGIC
+  );
+  attribute ORIG_REF_NAME : string;
+  attribute ORIG_REF_NAME of design_1_fir_IP_0_0_fir_IP_v1_0 : entity is "fir_IP_v1_0";
+end design_1_fir_IP_0_0_fir_IP_v1_0;
+
+architecture STRUCTURE of design_1_fir_IP_0_0_fir_IP_v1_0 is
+begin
+fir_IP_v1_0_S00_AXI_inst: entity work.design_1_fir_IP_0_0_fir_IP_v1_0_S00_AXI
+     port map (
+      s00_axi_aclk => s00_axi_aclk,
+      s00_axi_aresetn => s00_axi_aresetn,
+      s00_axi_arready => s00_axi_arready,
+      s00_axi_arvalid => s00_axi_arvalid,
+      s00_axi_awready => s00_axi_awready,
+      s00_axi_awvalid => s00_axi_awvalid,
+      s00_axi_bready => s00_axi_bready,
+      s00_axi_bvalid => s00_axi_bvalid,
+      s00_axi_rready => s00_axi_rready,
+      s00_axi_rvalid => s00_axi_rvalid,
+      s00_axi_wready => s00_axi_wready,
+      s00_axi_wvalid => s00_axi_wvalid
+    );
+end STRUCTURE;
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+library UNISIM;
+use UNISIM.VCOMPONENTS.ALL;
+entity design_1_fir_IP_0_0 is
+  port (
+    s00_axi_awaddr : in STD_LOGIC_VECTOR ( 3 downto 0 );
+    s00_axi_awprot : in STD_LOGIC_VECTOR ( 2 downto 0 );
+    s00_axi_awvalid : in STD_LOGIC;
+    s00_axi_awready : out STD_LOGIC;
+    s00_axi_wdata : in STD_LOGIC_VECTOR ( 31 downto 0 );
+    s00_axi_wstrb : in STD_LOGIC_VECTOR ( 3 downto 0 );
+    s00_axi_wvalid : in STD_LOGIC;
+    s00_axi_wready : out STD_LOGIC;
+    s00_axi_bresp : out STD_LOGIC_VECTOR ( 1 downto 0 );
+    s00_axi_bvalid : out STD_LOGIC;
+    s00_axi_bready : in STD_LOGIC;
+    s00_axi_araddr : in STD_LOGIC_VECTOR ( 3 downto 0 );
+    s00_axi_arprot : in STD_LOGIC_VECTOR ( 2 downto 0 );
+    s00_axi_arvalid : in STD_LOGIC;
+    s00_axi_arready : out STD_LOGIC;
+    s00_axi_rdata : out STD_LOGIC_VECTOR ( 31 downto 0 );
+    s00_axi_rresp : out STD_LOGIC_VECTOR ( 1 downto 0 );
+    s00_axi_rvalid : out STD_LOGIC;
+    s00_axi_rready : in STD_LOGIC;
+    s00_axi_aclk : in STD_LOGIC;
+    s00_axi_aresetn : in STD_LOGIC
+  );
+  attribute NotValidForBitStream : boolean;
+  attribute NotValidForBitStream of design_1_fir_IP_0_0 : entity is true;
+  attribute CHECK_LICENSE_TYPE : string;
+  attribute CHECK_LICENSE_TYPE of design_1_fir_IP_0_0 : entity is "design_1_fir_IP_0_0,fir_IP_v1_0,{}";
+  attribute downgradeipidentifiedwarnings : string;
+  attribute downgradeipidentifiedwarnings of design_1_fir_IP_0_0 : entity is "yes";
+  attribute x_core_info : string;
+  attribute x_core_info of design_1_fir_IP_0_0 : entity is "fir_IP_v1_0,Vivado 2018.2";
+end design_1_fir_IP_0_0;
+
+architecture STRUCTURE of design_1_fir_IP_0_0 is
+  signal \<const0>\ : STD_LOGIC;
+  attribute x_interface_info : string;
+  attribute x_interface_info of s00_axi_aclk : signal is "xilinx.com:signal:clock:1.0 S00_AXI_CLK CLK";
+  attribute x_interface_parameter : string;
+  attribute x_interface_parameter of s00_axi_aclk : signal is "XIL_INTERFACENAME S00_AXI_CLK, ASSOCIATED_BUSIF S00_AXI, ASSOCIATED_RESET s00_axi_aresetn, FREQ_HZ 100000000, PHASE 0.000, CLK_DOMAIN design_1_processing_system7_0_0_FCLK_CLK0";
+  attribute x_interface_info of s00_axi_aresetn : signal is "xilinx.com:signal:reset:1.0 S00_AXI_RST RST";
+  attribute x_interface_parameter of s00_axi_aresetn : signal is "XIL_INTERFACENAME S00_AXI_RST, POLARITY ACTIVE_LOW";
+  attribute x_interface_info of s00_axi_arready : signal is "xilinx.com:interface:aximm:1.0 S00_AXI ARREADY";
+  attribute x_interface_info of s00_axi_arvalid : signal is "xilinx.com:interface:aximm:1.0 S00_AXI ARVALID";
+  attribute x_interface_info of s00_axi_awready : signal is "xilinx.com:interface:aximm:1.0 S00_AXI AWREADY";
+  attribute x_interface_info of s00_axi_awvalid : signal is "xilinx.com:interface:aximm:1.0 S00_AXI AWVALID";
+  attribute x_interface_info of s00_axi_bready : signal is "xilinx.com:interface:aximm:1.0 S00_AXI BREADY";
+  attribute x_interface_info of s00_axi_bvalid : signal is "xilinx.com:interface:aximm:1.0 S00_AXI BVALID";
+  attribute x_interface_info of s00_axi_rready : signal is "xilinx.com:interface:aximm:1.0 S00_AXI RREADY";
+  attribute x_interface_info of s00_axi_rvalid : signal is "xilinx.com:interface:aximm:1.0 S00_AXI RVALID";
+  attribute x_interface_info of s00_axi_wready : signal is "xilinx.com:interface:aximm:1.0 S00_AXI WREADY";
+  attribute x_interface_info of s00_axi_wvalid : signal is "xilinx.com:interface:aximm:1.0 S00_AXI WVALID";
+  attribute x_interface_info of s00_axi_araddr : signal is "xilinx.com:interface:aximm:1.0 S00_AXI ARADDR";
+  attribute x_interface_info of s00_axi_arprot : signal is "xilinx.com:interface:aximm:1.0 S00_AXI ARPROT";
+  attribute x_interface_info of s00_axi_awaddr : signal is "xilinx.com:interface:aximm:1.0 S00_AXI AWADDR";
+  attribute x_interface_parameter of s00_axi_awaddr : signal is "XIL_INTERFACENAME S00_AXI, WIZ_DATA_WIDTH 32, WIZ_NUM_REG 4, SUPPORTS_NARROW_BURST 0, DATA_WIDTH 32, PROTOCOL AXI4LITE, FREQ_HZ 100000000, ID_WIDTH 0, ADDR_WIDTH 4, AWUSER_WIDTH 0, ARUSER_WIDTH 0, WUSER_WIDTH 0, RUSER_WIDTH 0, BUSER_WIDTH 0, READ_WRITE_MODE READ_WRITE, HAS_BURST 0, HAS_LOCK 0, HAS_PROT 1, HAS_CACHE 0, HAS_QOS 0, HAS_REGION 0, HAS_WSTRB 1, HAS_BRESP 1, HAS_RRESP 1, NUM_READ_OUTSTANDING 8, NUM_WRITE_OUTSTANDING 8, MAX_BURST_LENGTH 1, PHASE 0.000, CLK_DOMAIN design_1_processing_system7_0_0_FCLK_CLK0, NUM_READ_THREADS 4, NUM_WRITE_THREADS 4, RUSER_BITS_PER_BYTE 0, WUSER_BITS_PER_BYTE 0";
+  attribute x_interface_info of s00_axi_awprot : signal is "xilinx.com:interface:aximm:1.0 S00_AXI AWPROT";
+  attribute x_interface_info of s00_axi_bresp : signal is "xilinx.com:interface:aximm:1.0 S00_AXI BRESP";
+  attribute x_interface_info of s00_axi_rdata : signal is "xilinx.com:interface:aximm:1.0 S00_AXI RDATA";
+  attribute x_interface_info of s00_axi_rresp : signal is "xilinx.com:interface:aximm:1.0 S00_AXI RRESP";
+  attribute x_interface_info of s00_axi_wdata : signal is "xilinx.com:interface:aximm:1.0 S00_AXI WDATA";
+  attribute x_interface_info of s00_axi_wstrb : signal is "xilinx.com:interface:aximm:1.0 S00_AXI WSTRB";
+begin
+  s00_axi_bresp(1) <= \<const0>\;
+  s00_axi_bresp(0) <= \<const0>\;
+  s00_axi_rdata(31) <= \<const0>\;
+  s00_axi_rdata(30) <= \<const0>\;
+  s00_axi_rdata(29) <= \<const0>\;
+  s00_axi_rdata(28) <= \<const0>\;
+  s00_axi_rdata(27) <= \<const0>\;
+  s00_axi_rdata(26) <= \<const0>\;
+  s00_axi_rdata(25) <= \<const0>\;
+  s00_axi_rdata(24) <= \<const0>\;
+  s00_axi_rdata(23) <= \<const0>\;
+  s00_axi_rdata(22) <= \<const0>\;
+  s00_axi_rdata(21) <= \<const0>\;
+  s00_axi_rdata(20) <= \<const0>\;
+  s00_axi_rdata(19) <= \<const0>\;
+  s00_axi_rdata(18) <= \<const0>\;
+  s00_axi_rdata(17) <= \<const0>\;
+  s00_axi_rdata(16) <= \<const0>\;
+  s00_axi_rdata(15) <= \<const0>\;
+  s00_axi_rdata(14) <= \<const0>\;
+  s00_axi_rdata(13) <= \<const0>\;
+  s00_axi_rdata(12) <= \<const0>\;
+  s00_axi_rdata(11) <= \<const0>\;
+  s00_axi_rdata(10) <= \<const0>\;
+  s00_axi_rdata(9) <= \<const0>\;
+  s00_axi_rdata(8) <= \<const0>\;
+  s00_axi_rdata(7) <= \<const0>\;
+  s00_axi_rdata(6) <= \<const0>\;
+  s00_axi_rdata(5) <= \<const0>\;
+  s00_axi_rdata(4) <= \<const0>\;
+  s00_axi_rdata(3) <= \<const0>\;
+  s00_axi_rdata(2) <= \<const0>\;
+  s00_axi_rdata(1) <= \<const0>\;
+  s00_axi_rdata(0) <= \<const0>\;
+  s00_axi_rresp(1) <= \<const0>\;
+  s00_axi_rresp(0) <= \<const0>\;
+GND: unisim.vcomponents.GND
+     port map (
+      G => \<const0>\
+    );
+U0: entity work.design_1_fir_IP_0_0_fir_IP_v1_0
+     port map (
+      s00_axi_aclk => s00_axi_aclk,
+      s00_axi_aresetn => s00_axi_aresetn,
+      s00_axi_arready => s00_axi_arready,
+      s00_axi_arvalid => s00_axi_arvalid,
+      s00_axi_awready => s00_axi_awready,
+      s00_axi_awvalid => s00_axi_awvalid,
+      s00_axi_bready => s00_axi_bready,
+      s00_axi_bvalid => s00_axi_bvalid,
+      s00_axi_rready => s00_axi_rready,
+      s00_axi_rvalid => s00_axi_rvalid,
+      s00_axi_wready => s00_axi_wready,
+      s00_axi_wvalid => s00_axi_wvalid
+    );
+end STRUCTURE;
