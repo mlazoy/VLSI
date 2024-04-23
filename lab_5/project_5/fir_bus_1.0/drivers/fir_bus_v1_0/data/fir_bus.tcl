a0027ef80c5a60e5a977ad5d5c44c29dbe4c10a5
mlazoy <mlazoy@icloud.com>
2024-04-23 19:49:40 +0300

    AXI4 Interface

--- lab_5/project_5/fir_bus_1.0/drivers/fir_bus_v1_0/data/fir_bus.tcl
+++
@@ -1,5 +0,0 @@
-
-
-proc generate {drv_handle} {
-	xdefine_include_file $drv_handle "xparameters.h" "fir_bus" "NUM_INSTANCES" "DEVICE_ID"  "C_CPU_AXI_BASEADDR" "C_CPU_AXI_HIGHADDR" "C_FIR_AXI_BASEADDR" "C_FIR_AXI_HIGHADDR"
-}
