00f56548483d80407e651665e9b18c958fe3a630
mlazoy <mlazoy@icloud.com>
2024-05-14 12:48:07 +0300

    s2p working for text input done

--- lab_6/s2p_tb.vhd
+++ lab_6/s2p_tb.vhd
@@ -11,14 +11,17 @@ end s2p_tb;
 
 architecture Behavioral of s2p_tb is
 
+constant N_bits: integer:=5;
+
 component s2p_converter is
-port (clk, rst_n: in std_logic;
-  pixel_in: std_logic_vector(7 downto 0);
-  grid_out: out grid3x3;
-  valid_grid: out std_logic);
+port (clk, rst: in std_logic;
+      --row_number, pixel_number : in std_logic_vector(N_bits-1 downto 0);
+      pixel_in: std_logic_vector(7 downto 0);
+      grid_out: out grid3x3);
+--      valid_grid: out std_logic);
 end component;
 
-signal clk_tb, rst_n_tb, valid_grid_tb: std_logic;
+signal clk_tb, rst_tb, valid_grid_tb: std_logic;
 signal pixel_in_tb: std_logic_vector(7 downto 0);
 signal grid_out_tb: grid3x3;
 
@@ -29,21 +32,22 @@ shared variable pixel_from_file: std_logic_vector(7 downto 0);
 begin
 
 tst: s2p_converter port map(clk=>clk_tb,
-                            rst_n=>rst_n_tb,
+                            rst=>rst_tb,
                             pixel_in=>pixel_in_tb,
-                            grid_out=>grid_out_tb,
-                            valid_grid=>valid_grid_tb);
+                            grid_out=>grid_out_tb);
+--                            valid_grid=>valid_grid_tb);
 
 testing: process
 begin
 
-rst_n_tb<='1';
+rst_tb<='1';
 clk_tb<='0';
 wait for 10ns;
 clk_tb<='1';
 wait for 10ns;
 
-rst_n_tb<='0';
+rst_tb<='0';
+wait for 10ns;
 
 --for i in 0 to 32 loop
 --    for j in 0 to 32 loop
