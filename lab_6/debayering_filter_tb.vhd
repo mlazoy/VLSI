770d05ee1f4e71e5eaf2247162bf1716a77dbbeb
watchmaker23 <thanassis.spil@gmail.com>
2024-05-15 10:27:14 +0300

    write

--- lab_6/debayering_filter_tb.vhd
+++ lab_6/debayering_filter_tb.vhd
@@ -5,13 +5,14 @@ use ieee.numeric_std.all;
 use work.PXL_GRID.all; 
 use std.textio.all;
 use ieee.std_logic_textio.all;
+use ieee.math_real.all;
 
 entity debayering_filter_tb is
 end debayering_filter_tb;
 
 architecture sim of debayering_filter_tb is
 
-constant N_bits : integer := 5;
+constant N_bits : integer := 4;
 
 component debayering_filter is
     port (
@@ -26,8 +27,10 @@ end component;
 signal clk_tb, rst_tb, new_image_tb, valid_in_tb, image_finished_tb, valid_out_tb : std_logic;
 signal pixel_tb, R_tb, G_tb, B_tb : std_logic_vector(7 downto 0);
 
-file pixels_sample: text open read_mode is "sample_image.txt";
+file pixels_sample: text open read_mode is "bayer16x16_2024exam.txt";
+file output_file : text open write_mode is "results.txt";
 shared variable line_buff: line;
+shared variable line_buff_2 : line;
 shared variable pixel_from_file: std_logic_vector(7 downto 0);
 
 begin
@@ -68,15 +71,30 @@ while not endfile(pixels_sample) loop
     wait for 10ns;
     clk_tb<='1';
     wait for 10ns;
+
+    if (valid_out_tb = '1') then   
+        write(line_buff_2, to_integer(unsigned(R_tb)), left, 4);
+        write(line_buff_2, to_integer(unsigned(G_tb)), left, 4);
+        write(line_buff_2, to_integer(unsigned(B_tb)), left, 4);
+        writeline(output_file, line_buff_2);
+    end if;
 end loop;
+
+
 
 valid_in_tb <= '0';
 
-for i in 0 to 65 loop
+for i in 0 to 33 loop
     clk_tb<='0';
     wait for 10ns;
     clk_tb<='1';
     wait for 10ns;
+    if (valid_out_tb = '1') then   
+        write(line_buff_2, to_integer(unsigned(R_tb)), left, 4);
+        write(line_buff_2, to_integer(unsigned(G_tb)), left, 4);
+        write(line_buff_2, to_integer(unsigned(B_tb)), left, 4);
+        writeline(output_file, line_buff_2);
+    end if;
 end loop;
 
 wait;
