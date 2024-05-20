2dcf05d89d265e9ff47a94364a1417fb8a66cbcc
watchmaker23 <thanassis.spil@gmail.com>
2024-05-20 14:29:57 +0300

    from people that worked

---
+++ lab_7/top.vhd
@@ -0,0 +1,180 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
+entity dvlsi2021_lab5_top is
+  port (
+        DDR_cas_n         : inout STD_LOGIC;
+        DDR_cke           : inout STD_LOGIC;
+        DDR_ck_n          : inout STD_LOGIC;
+        DDR_ck_p          : inout STD_LOGIC;
+        DDR_cs_n          : inout STD_LOGIC;
+        DDR_reset_n       : inout STD_LOGIC;
+        DDR_odt           : inout STD_LOGIC;
+        DDR_ras_n         : inout STD_LOGIC;
+        DDR_we_n          : inout STD_LOGIC;
+        DDR_ba            : inout STD_LOGIC_VECTOR( 2 downto 0);
+        DDR_addr          : inout STD_LOGIC_VECTOR(14 downto 0);
+        DDR_dm            : inout STD_LOGIC_VECTOR( 3 downto 0);
+        DDR_dq            : inout STD_LOGIC_VECTOR(31 downto 0);
+        DDR_dqs_n         : inout STD_LOGIC_VECTOR( 3 downto 0);
+        DDR_dqs_p         : inout STD_LOGIC_VECTOR( 3 downto 0);
+        FIXED_IO_mio      : inout STD_LOGIC_VECTOR(53 downto 0);
+        FIXED_IO_ddr_vrn  : inout STD_LOGIC;
+        FIXED_IO_ddr_vrp  : inout STD_LOGIC;
+        FIXED_IO_ps_srstb : inout STD_LOGIC;
+        FIXED_IO_ps_clk   : inout STD_LOGIC;
+        FIXED_IO_ps_porb  : inout STD_LOGIC
+       );
+end entity; -- dvlsi2021_lab5_top
+
+architecture arch of dvlsi2021_lab5_top is
+
+  component design_1_wrapper is
+    port (
+          DDR_cas_n         : inout STD_LOGIC;
+          DDR_cke           : inout STD_LOGIC;
+          DDR_ck_n          : inout STD_LOGIC;
+          DDR_ck_p          : inout STD_LOGIC;
+          DDR_cs_n          : inout STD_LOGIC;
+          DDR_reset_n       : inout STD_LOGIC;
+          DDR_odt           : inout STD_LOGIC;
+          DDR_ras_n         : inout STD_LOGIC;
+          DDR_we_n          : inout STD_LOGIC;
+          DDR_ba            : inout STD_LOGIC_VECTOR( 2 downto 0);
+          DDR_addr          : inout STD_LOGIC_VECTOR(14 downto 0);
+          DDR_dm            : inout STD_LOGIC_VECTOR( 3 downto 0);
+          DDR_dq            : inout STD_LOGIC_VECTOR(31 downto 0);
+          DDR_dqs_n         : inout STD_LOGIC_VECTOR( 3 downto 0);
+          DDR_dqs_p         : inout STD_LOGIC_VECTOR( 3 downto 0);
+          FIXED_IO_mio      : inout STD_LOGIC_VECTOR(53 downto 0);
+          FIXED_IO_ddr_vrn  : inout STD_LOGIC;
+          FIXED_IO_ddr_vrp  : inout STD_LOGIC;
+          FIXED_IO_ps_srstb : inout STD_LOGIC;
+          FIXED_IO_ps_clk   : inout STD_LOGIC;
+          FIXED_IO_ps_porb  : inout STD_LOGIC;
+          --------------------------------------------------------------------------
+          ----------------------------------------------- PL (FPGA) COMMON INTERFACE
+          ACLK                                : out STD_LOGIC;
+          ARESETN                             : out STD_LOGIC_VECTOR(0 to 0);
+          ------------------------------------------------------------------------------------
+          -- PS2PL-DMA AXI4-STREAM MASTER INTERFACE TO ACCELERATOR AXI4-STREAM SLAVE INTERFACE
+          M_AXIS_TO_ACCELERATOR_tdata         : out STD_LOGIC_VECTOR(7 downto 0);
+          M_AXIS_TO_ACCELERATOR_tkeep         : out STD_LOGIC_VECTOR( 0    to 0);
+          M_AXIS_TO_ACCELERATOR_tlast         : out STD_LOGIC;
+          M_AXIS_TO_ACCELERATOR_tready        : in  STD_LOGIC;
+          M_AXIS_TO_ACCELERATOR_tvalid        : out STD_LOGIC;
+          ------------------------------------------------------------------------------------
+          -- ACCELERATOR AXI4-STREAM MASTER INTERFACE TO PL2P2-DMA AXI4-STREAM SLAVE INTERFACE
+          S_AXIS_S2MM_FROM_ACCELERATOR_tdata  : in  STD_LOGIC_VECTOR(31 downto 0);
+          S_AXIS_S2MM_FROM_ACCELERATOR_tkeep  : in  STD_LOGIC_VECTOR( 3 downto 0);
+          S_AXIS_S2MM_FROM_ACCELERATOR_tlast  : in  STD_LOGIC;
+          S_AXIS_S2MM_FROM_ACCELERATOR_tready : out STD_LOGIC;
+          S_AXIS_S2MM_FROM_ACCELERATOR_tvalid : in  STD_LOGIC
+         );
+  end component design_1_wrapper;
+  
+  component debayering_filter is
+    port (
+        clk, rst_n : in std_logic;
+        new_image, valid_in: in std_logic;
+        pixel : in std_logic_vector(7 downto 0);
+        image_finished, valid_out : out std_logic;
+        R, G, B : out std_logic_vector(7 downto 0)
+    );
+    end component;
+
+-------------------------------------------
+-- INTERNAL SIGNAL & COMPONENTS DECLARATION
+
+  signal aclk    : std_logic;
+  signal aresetn : std_logic_vector(0 to 0);
+
+  signal db_data_in  : std_logic_vector(7 downto 0);
+  signal db_data_out : std_logic_vector(31 downto 0);
+  signal db_tlast_ps_to_pl  : std_logic;
+  signal db_tlast_pl_to_ps  : std_logic;
+  signal db_tready_write : std_logic;
+  signal db_tready_read  : std_logic;
+  signal db_tvalid_write : std_logic;
+  signal db_tvalid_read  : std_logic;
+  signal tmp_keep_ps2pl : std_logic_vector(0 downto 0);
+  signal tmp_keep_pl2ps : std_logic_vector(3 downto 0);
+  --for our component
+  signal prev_tvalid, new_img_axi : std_logic;
+  signal Rout, Gout, Bout : std_logic_vector(7 downto 0);
+  
+
+begin
+
+  PROCESSING_SYSTEM_INSTANCE : design_1_wrapper
+    port map (
+              DDR_cas_n         => DDR_cas_n,
+              DDR_cke           => DDR_cke,
+              DDR_ck_n          => DDR_ck_n,
+              DDR_ck_p          => DDR_ck_p,
+              DDR_cs_n          => DDR_cs_n,
+              DDR_reset_n       => DDR_reset_n,
+              DDR_odt           => DDR_odt,
+              DDR_ras_n         => DDR_ras_n,
+              DDR_we_n          => DDR_we_n,
+              DDR_ba            => DDR_ba,
+              DDR_addr          => DDR_addr,
+              DDR_dm            => DDR_dm,
+              DDR_dq            => DDR_dq,
+              DDR_dqs_n         => DDR_dqs_n,
+              DDR_dqs_p         => DDR_dqs_p,
+              FIXED_IO_mio      => FIXED_IO_mio,
+              FIXED_IO_ddr_vrn  => FIXED_IO_ddr_vrn,
+              FIXED_IO_ddr_vrp  => FIXED_IO_ddr_vrp,
+              FIXED_IO_ps_srstb => FIXED_IO_ps_srstb,
+              FIXED_IO_ps_clk   => FIXED_IO_ps_clk,
+              FIXED_IO_ps_porb  => FIXED_IO_ps_porb,
+              --------------------------------------------------------------------------
+              ----------------------------------------------- PL (FPGA) COMMON INTERFACE
+              ACLK                                => aclk,    -- clock to accelerator
+              ARESETN                             => aresetn, -- reset to accelerator, active low
+              ------------------------------------------------------------------------------------
+              -- PS2PL-DMA AXI4-STREAM MASTER INTERFACE TO ACCELERATOR AXI4-STREAM SLAVE INTERFACE
+              M_AXIS_TO_ACCELERATOR_tdata         => db_data_in,
+              M_AXIS_TO_ACCELERATOR_tkeep         => tmp_keep_ps2pl,
+              M_AXIS_TO_ACCELERATOR_tlast         => db_tlast_ps_to_pl,
+              M_AXIS_TO_ACCELERATOR_tready        => db_tready_write,
+              M_AXIS_TO_ACCELERATOR_tvalid        => db_tvalid_write,
+              ------------------------------------------------------------------------------------
+              -- ACCELERATOR AXI4-STREAM MASTER INTERFACE TO PL2P2-DMA AXI4-STREAM SLAVE INTERFACE
+              S_AXIS_S2MM_FROM_ACCELERATOR_tdata  => db_data_out,
+              S_AXIS_S2MM_FROM_ACCELERATOR_tkeep  => tmp_keep_pl2ps,
+              S_AXIS_S2MM_FROM_ACCELERATOR_tlast  => db_tlast_pl_to_ps,
+              S_AXIS_S2MM_FROM_ACCELERATOR_tready => db_tready_read,
+              S_AXIS_S2MM_FROM_ACCELERATOR_tvalid => db_tvalid_read
+             );
+
+----------------------------
+-- COMPONENTS      
+db_tready_write <= '1';
+tmp_keep_pl2ps <= db_tvalid_read & db_tvalid_read & db_tvalid_read & db_tvalid_read;
+DEBAYER : debayering_filter port map (
+    clk => ACLK,
+    rst_n => ARESETN(0),
+    new_image => new_img_axi,
+    valid_in => db_tvalid_write,
+    pixel => db_data_in,
+    image_finished => db_tlast_pl_to_ps,
+    valid_out => db_tvalid_read,
+    R => Rout,
+    G => Gout,
+    B => Bout
+);
+db_data_out <= "00000000" & Rout & Gout & Bout;
+
+new_img_axi <= '1' when (db_tvalid_write = '1' and prev_tvalid = '0') else '0';
+process(ACLK)
+begin
+    if rising_edge(ACLK) then
+        prev_tvalid <= db_tvalid_write;
+    end if;
+end process;
+
+
+end architecture; -- arch
